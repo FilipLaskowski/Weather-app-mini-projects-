cdd438650116871af3fefb77ed23c02b6ba99fd3
unknown <laskowski.filip20@gmail.com>
2022-04-12 14:17:59 +0200

    added backgrounds

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const api = {
 
 function App() {
   // States
-  const [query, setQuery] = useState("Paris");
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({
     name: "None",
     sys: { country: "None" },
@@ -19,29 +19,57 @@ function App() {
     weather: [{ description: "None" }],
   });
   const [notFound, setNoFound] = useState();
+  const [bg, setBg] = useState("");
+  //functions
+  const bgChange = () => {
+    if (weather.weather[0].main === "Clear") {
+      setBg("clear-sky");
+    }
+    if (weather.weather[0].main === "Clouds") {
+      setBg("scattered-clouds");
+    }
+    if (weather.weather[0].main === "Rain") {
+      setBg("shower-rain");
+    }
+    if (weather.weather[0].main === "Drizzle") {
+      setBg("rain");
+    }
+    if (weather.weather[0].main === "Thunderstorm") {
+      setBg("thunderstorm");
+    }
+    if (weather.weather[0].main === "Snow") {
+      setBg("snow");
+    }
+    if (weather.weather[0].main === "Atmosphere") {
+      setBg("mist");
+    }
+  };
   // Effects
   useEffect(() => {
     const getWeather = async () => {
-      await fetch(
-        `${api.base}weather?q=${query}&units=metric&appid=${api.key}`
-      ).then((res) => {
-        if (res.ok) {
-          res.json().then((result) => setWeather(result));
-          setNoFound(false);
+      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`).then(
+        (res) => {
+          if (res.ok) {
+            res.json().then((result) => setWeather(result));
+            setNoFound(false);
+          }
+          if (res.status === 404) {
+            setNoFound(true);
+          }
+          if (res.status === 400) {
+            setNoFound(true);
+          }
         }
-        if (res.status === 404) {
-          setNoFound(true);
-        }
-        if (res.status === 400) {
-          setNoFound(true);
-        }
-      });
+      );
     };
     getWeather();
   }, [query]);
+  useEffect(() => {
+    bgChange();
+  }, [weather]);
 
   return (
-    <main>
+    <main className={bg}>
       <Search setQuery={setQuery} />
       {notFound ? <Error /> : <Weather weather={weather} />}
     </main>
